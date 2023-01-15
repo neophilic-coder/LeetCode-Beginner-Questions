6ad4e8809977ce5153272f2324748ce4a6bb52d4
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-01-15 19:53:31 +0530

    Update Longest Common Prefix.java

--- Longest Common Prefix.java
+++ Longest Common Prefix.java
@@ -3,6 +3,8 @@ class Solution {
     public String longestCommonPrefix(String[] strs) {
         if (strs == null || strs.length == 0)
                 return "";
+        
+        //sorting the array of strings
             Arrays.sort(strs);
             String initial = strs[0];
             String end = strs[strs.length - 1];
@@ -14,6 +16,8 @@ class Solution {
                 else
                     break;
             }
+        
+        //if condition result==0 true then initial substring is...
             return result == 0 ? "" : initial.substring(0, result);
     }
 }
