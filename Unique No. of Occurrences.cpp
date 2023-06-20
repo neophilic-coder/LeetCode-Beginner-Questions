e9b8facf433222e47c94947e314916241cb46f2b
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-06-20 16:41:46 +0530

    Create Unique No. of Occurrences.cpp

---
+++ Unique No. of Occurrences.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    bool uniqueOccurrences(vector<int>& arr) {
+        int count[2001]={0};
+        int visited[1001]={0};
+        for(int num: arr){
+            count[1000+num]++;
+        }
+        for(int i=0; i <2001 ; i++){
+            if(count[i]!=0){
+                if(visited[count[i]]==1){
+                    return false;
+                }
+                visited[count[i]]=1;
+            }
+        }
+        return true;
+        }
+};
