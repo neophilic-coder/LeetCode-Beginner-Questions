0bff2e27f52698f3976a985d3150469e0e0c91f5
Nandini Agarwal <98901861+neophilic-coder@users.noreply.github.com>
2023-06-25 19:41:05 +0530

    Reverse Integer.cpp

--- Reverse Integer.cpp
+++ Reverse Integer.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     int reverse(int x) {
         int ans=0;
+        
         while(x!=0){
             int digit=x%10;
             if((ans>INT_MAX/10) || (ans<INT_MIN/10)){
